45037055a35bd64d82f3f5819ba0c362f4e1bb50
Francisco Olea <folea@elementum.com>
2017-10-23 11:18:51 -0300

    Initial commit

---
+++ src/main/kotlin/org/weframe/kotlinresourcesserver/KotlinResourcesServerApplication.kt
@@ -0,0 +1,11 @@
+package org.weframe.kotlinresourcesserver
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KotlinResourcesServerApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(KotlinResourcesServerApplication::class.java, *args)
+}
